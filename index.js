758d2587d320fa7c335b29265f033b6572677e2d
Xuan <myatthiha.soe.9@gmail.com>
2025-01-20 16:48:06 +0630

    Ep-3-Global-Object

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+<<<<<<< HEAD
 console.log('Hello, MERN!');
 console.log('First Contribute');
+=======
+console.log(__dirname)
+console.log(__filename)
+global.setTimeout(function() {
+    console.log('Timeout executed')
+} , 4000)
+
+global.setInterval(() => {
+    console.log('Interval executed')
+}, 2000);
+
+>>>>>>> f63851c (Ep-3-Global-Object)
