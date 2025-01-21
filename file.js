2edf6910ab6a6294d64357b46c2688261e421c10
Xuan <myatthiha.soe.9@gmail.com>
2025-01-21 14:50:14 +0630

    Read Write Delete using fs default library

---
+++ file.js
@@ -0,0 +1,35 @@
+const fs = require('fs'); // default library for file system
+
+
+//write
+
+if (!fs.existsSync('./document/mernstack.txt')) {
+    fs.writeFile('./document/mernstack.txt', 'File write read testing', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+//delete
+else {
+    fs.unlink('./document/mernstack.txt', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('File deleted');
+        }
+    })
+}
+
+
+//read
+
+/*fs.readFile('./document/mernstack.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});     */
