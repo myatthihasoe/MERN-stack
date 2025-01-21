51992e7a63eee29f1c4ecb5b6ba40ce3b0c495b9
Xuan <myatthiha.soe.9@gmail.com>
2025-01-21 15:26:49 +0630

    Ep-5 Create Delete Folder

--- file.js
+++ file.js
@@ -3,25 +3,25 @@ const fs = require('fs'); // default library for file system
 
 //write
 
-if (!fs.existsSync('./document/mernstack.txt')) {
-    fs.writeFile('./document/mernstack.txt', 'File write read testing', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
-}
+// if (!fs.existsSync('./document/mernstack.txt')) {
+//     fs.writeFile('./document/mernstack.txt', 'File write read testing', (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(data);
+//         }
+//     });
+// }
 //delete
-else {
-    fs.unlink('./document/mernstack.txt', (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('File deleted');
-        }
-    })
-}
+// else {
+//     fs.unlink('./document/mernstack.txt', (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('File deleted');
+//         }
+//     })
+// }
 
 
 //read
@@ -33,3 +33,27 @@ else {
         console.log(data.toString());
     }
 });     */
+
+const path ='./newfolder';
+//folder delete
+if (fs.existsSync(path)) {
+    fs.rmdirSync(path, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Folder deleted');
+        }
+    });
+}
+
+//folder create
+else {
+    fs.mkdir(path, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Folder created');
+        }
+    
+    });
+}
