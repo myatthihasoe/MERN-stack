0e4f97e297666c4e062e49d3234f9b732b486daa
Xuan <myatthiha.soe.9@gmail.com>
2025-01-21 16:54:40 +0630

    Ep-6 Write Buffer large paragraph

--- stream.js
+++ stream.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./document/large.txt');
+const writeStream = fs.createWriteStream('./document/large-write.txt');
+// readStream.on('data', function (data) {
+    
+//     console.log(data.toString());
+//     console.log('-----chunk-----');
+// });
 
 readStream.on('data', function (data) {
     
-    console.log(data.toString());
-    console.log('-----chunk-----');
+    writeStream.write(data.toString());
+    writeStream.write('-----chunk-----');
 });
