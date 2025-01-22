e0f4fbb287b073b086e010f6187327da9b37a1d1
Xuan <myatthiha.soe.9@gmail.com>
2025-01-22 14:40:03 +0630

    Ep-8 Render html file to clients from node server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello World</h1>');
-    res.end();
+    fs.readFile('./views/home.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(3000,'localhost', () => {
