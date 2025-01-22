8b8efc548ef10643b30389988b3bc2bf6be75948
Xuan <myatthiha.soe.9@gmail.com>
2025-01-22 14:24:27 +0630

    Ep-7 Http Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello World</h1>');
+    res.end();
+});
+
+server.listen(3000,'localhost', () => {
+    console.log('Server listening on port 3000');
+});
