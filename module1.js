d65ec7186e579bcb5cacbcadd9f81f86223cfbf7
Xuan <myatthiha.soe.9@gmail.com>
2025-01-25 19:51:13 +0630

    Resolved merge conflicts

--- module1.js
+++
@@ -1,7 +0,0 @@
-const myname = "Myat Thiha"; // This is the module object
-const myage = 23;
-module.exports = {
-    myname,
-    myage
-}; // This is the module.exports object
-
--- module2.js
+++
@@ -1,6 +0,0 @@
-// const obj = require('./module1');
-
-// console.log(obj.name, obj.age); 
-
-const {myname, myage} = require('./module1');
-console.log(myname, myage); // This is the module object
